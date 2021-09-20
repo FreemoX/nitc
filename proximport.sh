9f2d239a7a1de9c6ceae4456fb5e9523e4b41010
FreemoX <frarol96@gmail.com>
2021-09-20 11:56:47 +0200

    Proximport v1.2.4.0
    Proximport now runs in a screen, added update confirm

--- proximport.sh
+++ proximport.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=1.2.3.0
+version=1.2.4.0
 
 # Proximport
 # A simple bash script that handles the importation of VM disks into a Proxmox VM
@@ -19,25 +19,7 @@ version=1.2.3.0
 # This software is released freely as-is; no guarantees or support is provided, use at your own discression!
 
 if [[ $1 = "--update" ]] || [[ $1 = "-u" ]]; then
-    wget https://raw.githubusercontent.com/FreemoX/nitc/main/proximport.sh -O proximport.sh.new && wait
-    versionNEW=$(head -2 proximport.sh.new | tail -1 | cut -d '=' -f 2)
-    versionNEWL=$(head -2 proximport.sh.new | tail -1 | cut -d '=' -f 2 | cut -d '.' -f 1)
-    versionNEWM=$(head -2 proximport.sh.new | tail -1 | cut -d '=' -f 2 | cut -d '.' -f 2)
-    versionNEWS=$(head -2 proximport.sh.new | tail -1 | cut -d '=' -f 2 | cut -d '.' -f 3)
-    versionL=$(head -2 proximport.sh | tail -1 | cut -d '=' -f 2 | cut -d '.' -f 1)
-    versionM=$(head -2 proximport.sh | tail -1 | cut -d '=' -f 2 | cut -d '.' -f 2)
-    versionS=$(head -2 proximport.sh | tail -1 | cut -d '=' -f 2 | cut -d '.' -f 3)
-    if [[ $versionNEWL -gt $versionL ]] || [[ $versionNEWM -gt $versionM ]] || [[ $versionNEWS -gt $versionS ]]; then
-        echo -e "\nThere is a new version available!\nCurrent version: $version\nNew version:     $versionNEW"
-        chmod +x proximport.sh.new && mv proximport.sh.new proximport.sh && echo -e "\nUpdate completed\n" || echo -e "\nUpdate failed\n"
-    elif [[ $versionNEWL -eq $versionL ]] && [[ $versionNEWM -eq $versionM ]] && [[ $versionNEWS -eq $versionS ]]; then
-        echo -e "\nUpdate not needed, already at the latest version!\nCurrent version: $version"
-        rm proximport.sh.new
-    else
-        echo -e "\nEn error occured while comparing the versions!\nThis is usually caused by the server not being able to reach GitHub\nThis can also be caused by running a version newer than the release on GitHub. You wizard..."
-        exit 1
-    fi
-    exit 0
+    runupdate
 elif [[ $1 = "--copy" ]] || [[ $1 = "-c" ]]; then
     mode=0
 elif [[ $1 = "--import" ]] || [[ $1 = "-i" ]]; then
@@ -59,6 +41,38 @@ else
     echo "-ci | --copy-import    Copy and import a VM disk from remote server"
     exit 0
 fi
+
+runupdate() {
+    wget https://raw.githubusercontent.com/FreemoX/nitc/main/proximport.sh -O proximport.sh.new && wait
+    versionNEW=$(head -2 proximport.sh.new | tail -1 | cut -d '=' -f 2)
+    versionNEWL=$(head -2 proximport.sh.new | tail -1 | cut -d '=' -f 2 | cut -d '.' -f 1)
+    versionNEWM=$(head -2 proximport.sh.new | tail -1 | cut -d '=' -f 2 | cut -d '.' -f 2)
+    versionNEWS=$(head -2 proximport.sh.new | tail -1 | cut -d '=' -f 2 | cut -d '.' -f 3)
+    versionL=$(head -2 proximport.sh | tail -1 | cut -d '=' -f 2 | cut -d '.' -f 1)
+    versionM=$(head -2 proximport.sh | tail -1 | cut -d '=' -f 2 | cut -d '.' -f 2)
+    versionS=$(head -2 proximport.sh | tail -1 | cut -d '=' -f 2 | cut -d '.' -f 3)
+    if [[ $versionNEWL -gt $versionL ]] || [[ $versionNEWM -gt $versionM ]] || [[ $versionNEWS -gt $versionS ]]; then
+        echo -e "\nThere is a new version available!\nCurrent version: $version\nNew version:     $versionNEW"
+        echo -e "\n\n"
+        read -p "Do you want to update proximport to $version? [y|n]" confirm
+        if [[ $confirm = "y" ]] || [[ $confirm = "Y" ]]; then
+            chmod +x proximport.sh.new && mv proximport.sh.new proximport.sh && echo -e "\nUpdate completed\n" || echo -e "\nUpdate failed\n"
+        else
+            echo "Input was not y|Y, not updating ..."
+        fi
+    elif [[ $versionNEWL -eq $versionL ]] && [[ $versionNEWM -eq $versionM ]] && [[ $versionNEWS -eq $versionS ]]; then
+        echo -e "\nUpdate not needed, already at the latest version!\nCurrent version: $version"
+        rm proximport.sh.new
+    else
+        echo -e "\nAn error occured while comparing the versions!\nThis is usually caused by the server not being able to reach GitHub\nThis can also be caused by running a version newer than the release on GitHub. You wizard..."
+        exit 1
+    fi
+    exit 0
+}
+
+makescreen() {
+    screen -S proximport.sh && echo -e "\nYou are now using a screen named \"proximport.sh\"\nTo disconnect from the screen, press \'CTRL+A D\'\nTo reconnect to the screen, run \'screen -r proximport.sh\'\n\nDO NOT PRESS CTRL+C or abort in any way, that will exit proximport and potentially lock the system!\n\n\n" || echo -e "\nUnable to create the screen \"proximport.sh\", is screen installed?\n\n"
+}
 
 getinfo() {
     if [[ "$mode" = 0 ]]; then
@@ -103,12 +117,15 @@ echopost() {
         echo -e "\n\nThe VM should now be imported into Proxmox.\nPlease note that some reconfiguration need to be done within the Proxmox WebGUI, such as:"
         echo -e "- Reconfigure the network interfaces\n    Imported VMs don't usually have network connection out-of-the-box\n    Editing \"/etc/netplan/01-netcfg.yaml\" to include the line \"renderer: networkd\" below the \"version\" line should enable the imported VM to grab DHCP\n    The original VM should be disabled to avoid MAC address conflicts and similar issues"
         echo -e "- Reconfigure the VM Boot Order so it attempts to boot from Disk 0 first\n    This is not strictly needed, but should avoid some issues and increase boot speed"
+        echo -e "\n"
     fi
     echo -e "\n\n"
     read -n 1 -s -p "Press any key to exit this program" confirm;echo -e "\n\n" && exit 0
 }
 
 main() {
+    runupdate
+    makescreen
     getinfo && echo -e "\nInformation gathered\n" || echo -e "\nInformation could not be gathered...\n"
     if [[ "$mode" = 0 ]] || [[ "$mode" = 2 ]]; then
         copyfiles && echo -e "\nFiles have been copied...\n" || echo -e "\nFiles could not be copied...\n"

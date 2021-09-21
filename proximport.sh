602bc23008458b7c9a643e0d18f405362dde710a
FreemoX <frarol96@gmail.com>
2021-09-21 10:39:17 +0200

    Proximport v1.2.5.0
    Added optional updates, proceed prompts, file- and success checks

--- proximport.sh
+++ proximport.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=1.2.4.0
+version=1.2.5.0
 
 # Proximport
 # A simple bash script that handles the importation of VM disks into a Proxmox VM
@@ -24,6 +24,7 @@ elif [[ $1 = "--copy" ]] || [[ $1 = "-c" ]]; then
     mode=0
 elif [[ $1 = "--import" ]] || [[ $1 = "-i" ]]; then
     mode=1
+    copysuccess=1
 elif [[ $1 = "--copy-import" ]] || [[ $1 = "-ci" ]]; then
     echo -e "\nNOTE: using --copy-import might take too long and cause issues\nIt's recommended to do one run with --copy before doing another run with --import\n\nRunning \"--copy-import\" is only adviced on VM disks < 10GB\n"
     sleep 5
@@ -48,26 +49,28 @@ runupdate() {
     versionNEWL=$(head -2 proximport.sh.new | tail -1 | cut -d '=' -f 2 | cut -d '.' -f 1)
     versionNEWM=$(head -2 proximport.sh.new | tail -1 | cut -d '=' -f 2 | cut -d '.' -f 2)
     versionNEWS=$(head -2 proximport.sh.new | tail -1 | cut -d '=' -f 2 | cut -d '.' -f 3)
+    versionNEWO=$(head -2 proximport.sh.new | tail -1 | cut -d '=' -f 2 | cut -d '.' -f 4)
     versionL=$(head -2 proximport.sh | tail -1 | cut -d '=' -f 2 | cut -d '.' -f 1)
     versionM=$(head -2 proximport.sh | tail -1 | cut -d '=' -f 2 | cut -d '.' -f 2)
     versionS=$(head -2 proximport.sh | tail -1 | cut -d '=' -f 2 | cut -d '.' -f 3)
-    if [[ $versionNEWL -gt $versionL ]] || [[ $versionNEWM -gt $versionM ]] || [[ $versionNEWS -gt $versionS ]]; then
+    versionO=$(head -2 proximport.sh | tail -1 | cut -d '=' -f 2 | cut -d '.' -f 4)
+    if [[ $versionNEWL -gt $versionL ]] || [[ $versionNEWM -gt $versionM ]] || [[ $versionNEWS -gt $versionS ]] || [[ $versionNEWO -gt $versionO ]]; then
         echo -e "\nThere is a new version available!\nCurrent version: $version\nNew version:     $versionNEW"
         echo -e "\n\n"
         read -p "Do you want to update proximport to $version? [y|n]" confirm
         if [[ $confirm = "y" ]] || [[ $confirm = "Y" ]]; then
             chmod +x proximport.sh.new && mv proximport.sh.new proximport.sh && echo -e "\nUpdate completed\n" || echo -e "\nUpdate failed\n"
+            echo "A restart of proximport is needed to apply the updates!"
+            exit 0
         else
             echo "Input was not y|Y, not updating ..."
         fi
     elif [[ $versionNEWL -eq $versionL ]] && [[ $versionNEWM -eq $versionM ]] && [[ $versionNEWS -eq $versionS ]]; then
         echo -e "\nUpdate not needed, already at the latest version!\nCurrent version: $version"
-        rm proximport.sh.new
+        rm proximport.sh.new && wait
     else
-        echo -e "\nAn error occured while comparing the versions!\nThis is usually caused by the server not being able to reach GitHub\nThis can also be caused by running a version newer than the release on GitHub. You wizard..."
-        exit 1
+        echo -e "\nAn error occured while comparing the versions!\nThis is usually caused by the server not being able to reach GitHub\nThis can also be caused by running a version newer than the release on GitHub. You wizard..." && sleep 1
     fi
-    exit 0
 }
 
 makescreen() {
@@ -81,12 +84,33 @@ getinfo() {
         read -p "Username with access to the file: " remoteuser
         read -p "FULL file path on the remote server: " remotefile
         read -p "FULL file path for the transfered file (on this system): " localfile
+        if [[ -f "$localfile" ]]; then
+            echo -e "That file already exists!\nIt needs to be removed in order to proceed!"
+            read -p "Remove $localfile ? [y|n]"
+            if [[ $confirm = "y" ]] || [[ $confirm = "Y" ]]; then
+                echo "Removing $localfile ..."
+                sudo rm $localfile && wait
+            else
+                echo "Input was not y|Y, aborting ..."
+                exit 0
+            fi
+        fi
         echo -e "\nInformation you provided:\nServer IP: $remoteip\nRemote User: $remoteuser\nFile to copy FROM: $remotefile\nFile to copy TO: $localfile"
+        read -p "Is the information above correct?" confirm
+        if [[ $confirm != "y" ]] || [[ $confirm != "Y" ]]; then
+            echo -e "Input was not y|Y ...\n"
+            getinfo
+        fi
     elif [[ "$mode" = 1 ]]; then
         read -p "FULL file path for the transfered file (on this system): " localfile
         read -p "Proxmox VM ID: " vmid
         read -p "Proxmox Storage Pool: " localpool
         echo -e "\nInformation you provided:\nVM Disk to import: $localfile\nVM ID: $vmid\nProxmox storage pool: $localpool"
+        read -p "Is the information above correct?" confirm
+        if [[ $confirm != "y" ]] || [[ $confirm != "Y" ]]; then
+            echo -e "Input was not y|Y ...\n"
+            getinfo
+        fi
     elif [[ "$mode" = 2 ]]; then
         echo -e "\n\nPlease supply the following information for the file copy:\n"
         read -p "Server IP to copy from: " remoteip
@@ -96,28 +120,57 @@ getinfo() {
         read -p "Proxmox VM ID: " vmid
         read -p "Proxmox Storage Pool: " localpool
         echo -e "\nInformation you provided:\nServer IP: $remoteip\nRemote User: $remoteuser\nFile to copy FROM: $remotefile\nFile to copy TO: $localfile\nVM ID: $vmid\nProxmox storage pool: $localpool"
+        read -p "Is the information above correct?" confirm
+        if [[ $confirm != "y" ]] || [[ $confirm != "Y" ]]; then
+            echo -e "Input was not y|Y ...\n"
+            getinfo
+        fi
     fi
 }
 
 copyfiles() {
     echo -e "\nCopying the file \"$remotefile\" from \"$remoteip\" with the user \"$remoteuser\"...\n"
-    scp "$remoteuser"@"$remoteip":"$remotefile" "$localfile"
+    scp "$remoteuser"@"$remoteip":"$remotefile" "$localfile" && wait && copysuccess=1
 }
 
 importvm() {
-    sudo qm importdisk "$vmid" "$localfile" "$localpool" && wait
-    sudo qm rescan && wait
-    sudo qm set "$vmid" --scsi0 "$localpool":vm-"$vmid"-disk-0
+    sudo qm importdisk "$vmid" "$localfile" "$localpool" && wait && importdisksuccess=1
+    sudo qm rescan && wait && rescansuccess=1
+    sudo qm set "$vmid" --scsi0 "$localpool":vm-"$vmid"-disk-0 && setvmmaindisk=1
 }
 
 echopost() {
     if [[ "$mode" = 0 ]]; then
-        echo -e "The VM Disk should now be copied from the remote server, and stored on this server under:\n$localfile\n\nRemember to re-run Proximport with the \"--import\" argument to start the import"
+        if [[ $copysuccess -ne 1 ]]; then
+            echo -e "\n\nTRANSFER ERROR: The VM disk file could not be copied from the remote server!\n\n"
+        elif [[ $copysuccess -eq 1 ]]; then
+            echo -e "The VM Disk should now be copied from the remote server, and stored on this server under:\n$localfile\n\nRemember to re-run Proximport with the \"--import\" argument to start the import"
+        else
+            echo -e "\n\nUNKNOWN ERROR: An unknown error occured!\n\n"
+        fi
     elif [[ "$mode" = 1 ]] || [[ "$mode" = 2 ]]; then
-        echo -e "\n\nThe VM should now be imported into Proxmox.\nPlease note that some reconfiguration need to be done within the Proxmox WebGUI, such as:"
-        echo -e "- Reconfigure the network interfaces\n    Imported VMs don't usually have network connection out-of-the-box\n    Editing \"/etc/netplan/01-netcfg.yaml\" to include the line \"renderer: networkd\" below the \"version\" line should enable the imported VM to grab DHCP\n    The original VM should be disabled to avoid MAC address conflicts and similar issues"
-        echo -e "- Reconfigure the VM Boot Order so it attempts to boot from Disk 0 first\n    This is not strictly needed, but should avoid some issues and increase boot speed"
-        echo -e "\n"
+        if [[ $copysuccess -ne 1 ]] || [[ $importdisksuccess -ne 1 ]] || [[ $rescansuccess -ne 1 ]] || [[ $setvmmaindisk -ne 1 ]]; then
+            echo -e "\n\nERROR: An error occured during the operation....\nThe following tasks returned a failed flag:\n"
+            if [[ $copysuccess -ne 1 ]]; then
+                echo "- File transfer from remote server"
+            fi
+            if [[ $importdisksuccess -ne 1 ]]; then
+                echo "- Importing disk image to Proxmox"
+            fi
+            if [[ $rescansuccess -ne 1 ]]; then
+                echo "- Rescanning Proxmox disks"
+            fi
+            if [[ $setvmmaindisk -ne 1 ]]; then
+                echo "- Assigning the imported disk as the main disk"
+            fi
+            echo -e "NOTE: The top failed task indicates which task in the chain failed"
+        elif [[ $copysuccess -eq 1 ]] && [[ $importdisksuccess -eq 1 ]] && [[ $rescansuccess -eq 1 ]] && [[ $setvmmaindisk -eq 1 ]]; then
+            echo -e "\n\nThe VM should now be imported into Proxmox.\nPlease note that some reconfiguration need to be done within the Proxmox WebGUI, such as:"
+            echo -e "- Reconfigure the network interfaces\n    Imported VMs don't usually have network connection out-of-the-box\n    Editing \"/etc/netplan/01-netcfg.yaml\" to include the line \"renderer: networkd\" below the \"version\" line should enable the imported VM to grab DHCP\n    The original VM should be disabled to avoid MAC address conflicts and similar issues"
+            echo -e "- Reconfigure the VM Boot Order so it attempts to boot from Disk 0 first\n    This is not strictly needed, but should avoid some issues and increase boot speed"
+            echo -e "\n"
+        else
+            echo -e "\n\nUNKNOWN ERROR: An unknown error occured!\n\n"
     fi
     echo -e "\n\n"
     read -n 1 -s -p "Press any key to exit this program" confirm;echo -e "\n\n" && exit 0
